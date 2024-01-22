2ad37e59651ac3e0815e879ffd631e81c97a44e3
bogil SO <sotomas232@gmail.com>
2024-01-23 00:37:24 +0900

    proxy design

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,20 +1,42 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
     public static void main(String[] args) {
-
+        /*
         IBrowser browser = new BrowserProxy("www.naver.com");
         browser.show();
         browser.show();
         browser.show();
+        */
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
 
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+                );
+
+        aopBrowser.show();
+        System.out.println("time: "+end.get());
+
+        aopBrowser.show();
+        System.out.println("time: "+end.get());
 
     }
 
